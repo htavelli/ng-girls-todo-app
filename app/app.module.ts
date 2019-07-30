0fd825de9dc010b728875d63441c9b44b18474e3
htavelli <47439362+htavelli@users.noreply.github.com>
2019-07-30 00:03:15 -0500

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { ListManagerComponent } from './list-manager/list-manager.component';
+import { TodoListService } from './services/todo-list.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputButtonUnitComponent,
+    TodoItemComponent,
+    ListManagerComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    TodoListService,
+    Storage
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
